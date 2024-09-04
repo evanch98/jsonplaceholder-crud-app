39e971d1d1d151b8ce12b3cb150ad29990812147
Kyaw Thu <evanch98@gmail.com>
2024-09-04 20:34:04 +0800

    updating the useDeleteModal

--- src/store/use-delete-modal.ts
+++ src/store/use-delete-modal.ts
@@ -1,14 +1,16 @@
 import { create } from 'zustand';
 
 type UseDeleteModalStore = {
+  id?: number;
   isOpen: boolean;
-  onOpen: () => void;
+  // eslint-disable-next-line no-unused-vars
+  onOpen: (id: number) => void;
   onClose: () => void;
 };
 
 const useDeleteModal = create<UseDeleteModalStore>((set) => ({
   isOpen: false,
-  onOpen: () => set({ isOpen: true }),
+  onOpen: (id) => set({ isOpen: true, id }),
   onClose: () => set({ isOpen: false }),
 }));
 
